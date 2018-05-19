b86c46fbcef568977223b2db9744c688321d6d50
Pascal <unrundead_designs@gmx.de>
2018-05-19 10:10:01 +0200

    Added Class

---
+++ CSharpSaltHashSHA256/CSharpSaltHashSHA256/Crypto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSaltHashSHA256
+{
+    public static class Crypto
+    {
+        private static String ByteArrayToHexString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+
+            return hex.ToString();
+        }
+
+        private static String CreateSalt(int length)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buff = new byte[length];
+            rng.GetBytes(buff);
+
+            return Convert.ToBase64String(buff);
+        }
+
+        public static String HashToSHA256(string password, string salt)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(password + salt);
+            System.Security.Cryptography.SHA256Managed sha256hashstring = new System.Security.Cryptography.SHA256Managed();
+
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            return ByteArrayToHexString(hash);
+        }
+    }
+}
